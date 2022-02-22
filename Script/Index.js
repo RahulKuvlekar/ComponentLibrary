156d4031a3473982b4ec6b24420025081e8fb03a
RAHUL KUVLEKAR <75781543+RahulKuvlekar@users.noreply.github.com>
2022-02-22 17:06:38 +0530

    Update - Responsive navigation bar

---
+++ Script/Index.js
@@ -0,0 +1,9 @@
+const navPills = document.querySelectorAll(".nav-pill");
+const btnBurger = document.querySelector(".burger-menu");
+
+btnBurger.addEventListener("click", () => {
+  navPills.forEach((elementTag) => {
+    elementTag.classList.toggle("nav-active");
+    btnBurger.classList.toggle("nav-active");
+  });
+});
